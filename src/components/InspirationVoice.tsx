11c8904630a6d2d061a77ca6a150cc75d290bd24
ishaangupta122 <ishaang2209@gmail.com>
2025-04-15 13:05:42 +0530

    init

--- src/components/InspirationVoice.tsx
+++ src/components/InspirationVoice.tsx
@@ -21,7 +21,7 @@ const InspirationVoice = () => {
       name: "Sh. Ravinder Singh Hundal",
       description: `Deputy Director,
        DTE Punjab`,
-      image: "/user_icon.webp",
+      image: "/ravinder_hundal_rounded.jpg",
     },
     {
       title: "Deputy Director",
@@ -34,7 +34,7 @@ const InspirationVoice = () => {
       title: "Registrar-cum-Controller of Exams",
       name: "Sh. Sanjeev Goyal ",
       description: "Registrar-cum-Controller of Exams PSBTE Chandigarh",
-      image: "/user_icon.webp",
+      image: "/Sanjeev_Jain_rounded.jpg",
     },
     {
       title: "Vice Chairman",
@@ -44,18 +44,17 @@ const InspirationVoice = () => {
       image: "/vice_chairman_rounded.png",
     },
     {
-      title: "Principal",
-      name: "DR. ANKUSH KANSAL",
-      description: "Principal, Thapar Polytechnic College, Patiala, Punjab",
-      image: "/principal_rounded.png",
-    },
-
-    {
       title: "Coordinator",
       name: "DR. DWARIKA NATH RATHA",
       description:
         "Professor, Civil Engineering, TIET, Coordinator, Thapar Polytechnic College",
       image: "/coordinator_rounded.png",
+    },
+    {
+      title: "Principal",
+      name: "DR. ANKUSH KANSAL",
+      description: "Principal, Thapar Polytechnic College, Patiala, Punjab",
+      image: "/principal_rounded.png",
     },
   ]);
 
@@ -85,13 +84,13 @@ const InspirationVoice = () => {
                 />
               </div>
               <div className="w-full md:w-1/2 md:text-left">
-                <p className="playfair text-3xl tracking-tight">
-                  <span className="italic">{voice.title}</span>
-                </p>
-                <p className="mt-3 text-lg font-medium uppercase">
+                <p className="mt-3 text-2xl tracking-tight uppercase">
                   - {voice.name}
                 </p>
-                <p className="opacity-70 mt-3 font-light whitespace-pre-line">
+                {/* <p className="playfair text-xl tracking-tight">
+                  <span className="italic">{voice.title}</span>
+                </p> */}
+                <p className="text-lg  opacity-70 mt-3 font-light whitespace-pre-line">
                   {voice.description}
                 </p>
               </div>
