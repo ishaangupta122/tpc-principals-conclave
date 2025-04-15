dd13194a0cddc0e9eb7dba17c446fd0ce9145327
ishaangupta122 <ishaang2209@gmail.com>
2025-04-15 15:50:11 +0530

    removed phone no.

--- src/components/InspirationVoice.tsx
+++ src/components/InspirationVoice.tsx
@@ -1,50 +1,48 @@
-import { voices } from '../data';
+import { voices } from "../data";
 
 const InspirationVoice = () => {
-	return (
-		<div
-			className='py-20'
-			style={{
-				background: 'url(./dark-bg.jpg)',
-				backgroundRepeat: 'no-repeat',
-				backgroundSize: 'cover',
-			}}
-		>
-			<div className='mx-auto container max-w-7xl text-white md:px-10 px-5'>
-				<p className='text-4xl md:text-5xl font-light'>
-					<span className='playfair italic'>Voices of</span> <br /> INSPIRATION
-				</p>
+  return (
+    <div
+      className="py-20"
+      style={{
+        background: "url(./dark-bg.jpg)",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}>
+      <div className="mx-auto container max-w-7xl text-white md:px-10 px-5">
+        <p className="text-4xl md:text-5xl font-light">
+          <span className="playfair italic">Voices of</span> <br /> INSPIRATION
+        </p>
 
-				<div className='mt-20 grid lg:grid-cols-2 grid-cols-1 gap-x-6 gap-y-24'>
-					{voices.map((voice, index) => (
-						<div
-							key={index}
-							className='flex items-center justify-end flex-col md:flex-row text-right gap-4'
-						>
-							<div className='flex justify-end md:block w-full md:w-1/3'>
-								<img
-									src={voice.image}
-									alt={voice.name}
-									className='h-44 w-fit object-contain rounded-full'
-								/>
-							</div>
-							<div className='w-full md:w-1/2 md:text-left'>
-								<p className='mt-3 text-2xl tracking-tight uppercase'>
-									{voice.name}
-								</p>
-								{/* <p className="playfair text-xl tracking-tight">
+        <div className="mt-20 grid lg:grid-cols-2 grid-cols-1 gap-x-6 gap-y-24">
+          {voices.map((voice, index) => (
+            <div
+              key={index}
+              className="flex items-center justify-start flex-col md:flex-row text-left gap-4">
+              <div className="flex md:block w-full md:w-1/3">
+                <img
+                  src={voice.image}
+                  alt={voice.name}
+                  className="h-44 w-fit object-contain rounded-full"
+                />
+              </div>
+              <div className="w-full md:w-1/2 md:text-left">
+                <p className="mt-3 text-2xl tracking-tight uppercase">
+                  {voice.name}
+                </p>
+                {/* <p className="playfair text-xl tracking-tight">
                   <span className="italic">{voice.title}</span>
                 </p> */}
-								<p className='text-lg  opacity-70 mt-3 font-light whitespace-pre-line'>
-									{voice.description}
-								</p>
-							</div>
-						</div>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+                <p className="text-lg  opacity-70 mt-3 font-light whitespace-pre-line">
+                  {voice.description}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default InspirationVoice;
--- src/data.ts
+++ src/data.ts
@@ -1,55 +1,55 @@
 export const voices = [
-	{
-		title: 'Chairman',
-		name: 'SH. ANIL BHARGAVA',
-		description:
-			'Chairman, Managing Committee, Thapar Polytechnic College, Patiala',
-		image: '/chairman_rounded.png',
-	},
-	{
-		title: 'Director',
-		name: 'Dr. Padmakumar Nair',
-		description:
-			'Director, Thapar Institute of Engineering & Technology Patiala, Punjab',
-		image: '/nair_rounded.jpg',
-	},
-	{
-		title: 'Deputy Director',
-		name: 'Sh. Ravinder Singh Hundal',
-		description: `Deputy Director, DTE Punjab`,
-		image: '/ravinder_hundal_rounded.jpg',
-	},
-	{
-		title: 'Deputy Director',
-		name: 'Dr. Ajay Batish',
-		description:
-			'Deputy Director, Thapar Institute of Engineering & Technology Patiala, Punjab',
-		image: '/batish_rounded.jpg',
-	},
-	{
-		title: 'Registrar-cum-Controller of Exams',
-		name: 'Sh. Sanjeev Goyal ',
-		description: 'Registrar-cum-Controller of Exams PSBTE Chandigarh',
-		image: '/Sanjeev_Goyal_rounded.jpg',
-	},
-	{
-		title: 'Vice Chairman',
-		name: 'DR. GURBINDER SINGH',
-		description:
-			'Vice Chairman, Managing Committee TPC, Registrar, TIET, Director LMTSM',
-		image: '/vice_chairman_rounded.png',
-	},
-	{
-		title: 'Coordinator',
-		name: 'DR. DWARIKA NATH RATHA',
-		description:
-			'Professor, Civil Engineering, TIET, Coordinator, Thapar Polytechnic College',
-		image: '/coordinator_rounded.png',
-	},
-	{
-		title: 'Principal',
-		name: 'DR. ANKUSH KANSAL',
-		description: 'Principal, Thapar Polytechnic College, Patiala, Punjab',
-		image: '/principal_rounded.png',
-	},
+  {
+    title: "Chairman",
+    name: "SH. ANIL BHARGAVA",
+    description:
+      "Chairman, Managing Committee, Thapar Polytechnic College, Patiala",
+    image: "/chairman_rounded.png",
+  },
+  {
+    title: "Director",
+    name: "Dr. Padmakumar Nair",
+    description:
+      "Director, Thapar Institute of Engineering & Technology Patiala, Punjab",
+    image: "/nair_rounded.jpg",
+  },
+  {
+    title: "Deputy Director",
+    name: "Sh. Ravinder Singh Hundal",
+    description: `Deputy Director, DTE Punjab`,
+    image: "/ravinder_hundal_rounded.jpg",
+  },
+  {
+    title: "Deputy Director",
+    name: "Dr. Ajay Batish",
+    description:
+      "Deputy Director, Thapar Institute of Engineering & Technology Patiala, Punjab",
+    image: "/batish_rounded.jpg",
+  },
+  {
+    title: "Registrar-cum-Controller of Exams",
+    name: "Sh. Sanjeev Goyal ",
+    description: "Registrar-cum-Controller of Exams PSBTE&IT Chandigarh",
+    image: "/Sanjeev_Goyal_rounded.jpg",
+  },
+  {
+    title: "Vice Chairman",
+    name: "DR. GURBINDER SINGH",
+    description:
+      "Vice Chairman, Managing Committee TPC, Registrar, TIET, Director LMTSM",
+    image: "/vice_chairman_rounded.png",
+  },
+  {
+    title: "Coordinator",
+    name: "DR. DWARIKA NATH RATHA",
+    description:
+      "Professor, Civil Engineering, TIET, Coordinator, Thapar Polytechnic College",
+    image: "/coordinator_rounded.png",
+  },
+  {
+    title: "Principal",
+    name: "DR. ANKUSH KANSAL",
+    description: "Principal, Thapar Polytechnic College, Patiala, Punjab",
+    image: "/principal_rounded.png",
+  },
 ];
